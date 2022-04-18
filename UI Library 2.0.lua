6a2ff6cb71b96fa259442c959630c0d37acac246
PurplMochii <98187845+ghastlytim@users.noreply.github.com>
2022-04-17 21:33:27 -0400

    Update UI Library 2.0.lua

--- UI Library 2.0.lua
+++ UI Library 2.0.lua
@@ -27,9 +27,9 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
-    BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0, 85, 255);
+    MainColor = Color3.fromRGB(0, 0, 0);
+    BackgroundColor = Color3.fromRGB(0, 0, 0);
+    AccentColor = Color3.fromRGB(116, 73, 227);
     OutlineColor = Color3.fromRGB(50, 50, 50);
 
     Black = Color3.new(0, 0, 0);
