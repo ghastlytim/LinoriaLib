05983f67723ca186fcca69654124978c3dba157b
PurplMochii <98187845+ghastlytim@users.noreply.github.com>
2022-04-21 10:18:45 -0400

    Update Examplev2.lua

--- Examplev2.lua
+++ Examplev2.lua
@@ -1,5 +1,6 @@
 Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/ghastlytim/LinoriaLib/main/linorialibedited.lua'))();
 
+local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local Mouse = LocalPlayer:GetMouse()
 
