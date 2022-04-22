d7373c8bc8265ad2e1b4ca5ad2f0f24b94cc6651
PurplMochii <98187845+ghastlytim@users.noreply.github.com>
2022-04-22 11:39:07 -0400

    Update linorialibedited.lua

--- linorialibedited.lua
+++ linorialibedited.lua
@@ -31,7 +31,7 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(0, 0, 0);
     BackgroundColor = Color3.fromRGB(0, 0, 0);
-    AccentColor = Color3.fromRGB(255,255.255);
+    AccentColor = Color3.fromRGB(255,255,255);
     OutlineColor = Color3.fromRGB(50, 50, 50);
 
     Black = Color3.new(0, 0, 0);
