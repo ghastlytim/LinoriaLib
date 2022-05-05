f12df9838ce7a5ec3085480e5f922b7b5b6095b3
PurplMochii <98187845+ghastlytim@users.noreply.github.com>
2022-05-05 07:41:08 -0400

    Update Examplev2.lua

--- Examplev2.lua
+++ Examplev2.lua
@@ -36,12 +36,14 @@ local function UpdateTheme()
 end;
 
 local function SetDefault()
+if Flags.Selectedtheme.Value == "Default" then
     Flags.FontColor:SetValueRGB(Color3.fromRGB(255, 255, 255));
     Flags.MainColor:SetValueRGB(Color3.fromRGB(28, 28, 28));
     Flags.BackgroundColor:SetValueRGB(Color3.fromRGB(20, 20, 20));
     Flags.AccentColor:SetValueRGB(Color3.fromRGB(0, 85, 255));
     Flags.OutlineColor:SetValueRGB(Color3.fromRGB(50, 50, 50));
     Flags.Rainbow:SetValue(false);
+else if
 
     UpdateTheme();
 end;
